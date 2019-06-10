1410b5c9febdd03a15434073cc9c819f4074ea32
kiran <kbatchu2016@gmail.com>
2019-06-10 10:31:56 +0530

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("this is from the app");
+            Console.WriteLine("this is from the app from jenkins");
         }
     }
 }
